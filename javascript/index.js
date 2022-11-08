20c14624953288d5a8c2caf60795b9c001c3067f
DanielRomero90 <danielromeropm@gmail.com>
2022-11-07 21:39:12 -0300

    act 7/11

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+let nombre = "Daniel";
+let apellido = "Romero";
+let edad = 31;
+let mascota = "Gato:Anakin";
+
+/**console.log(typeof nombre);
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(mascota);*/
+
+let numeroA = 5; 
+let numeroB = 10;
+let resultadoSuma = numeroA + numeroB;
+
+console.log(resultadoSuma);
+
+
+/**let nombreIngresado = prompt("Ingrese su nombre");
+*/
+/**alert("Hola mundo");*/
+
+let entrada = prompt("ingresar una letra");
+let salida = entrada + " " + "ingresada";
+alert(salida);
